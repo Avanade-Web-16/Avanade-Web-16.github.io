49f458ec268736a67a52b45ffe500d0bfd12eec1
Marianna Magno Nascimento <mari.magno.osorio@avanade.com>
2022-10-05 15:03:43 -0300

    Deploy

--- assets/js/fetch.js
+++ assets/js/fetch.js
@@ -1,13 +1,26 @@
 const cep = document.querySelector("#cep");
 
+const showData = async (result) => {
+
+  for (const campo in result) {
+    if (document.querySelector("#" + campo)) {
+      document.querySelector("#" + campo).value = result[campo];
+    }
+  }
+};
+
 //o evento blur significa que clicamos na caixa de texto e saímos dela.
-cep.addEventListener("blur", (e) => {
+cep.addEventListener("blur", async (e) => {
   let search = cep.value.replace("-", "");
+  if (search < 8 || Number(search) === NaN) {
+    alert("CEP inválido!");
+    return;
+  }
 
   const options = {
-    method: "GET",
-    mode: "cors",
-    cache: "default",
+    method: "GET", //significa que o dados vão por url.
+    mode: "cors", //2 significados. voce quanto usuario, tem uma importante. e a url importante tem outra importancia.
+    cache: "default", //
   };
 
   // https://viacep.com.br/ws/01001000/json/
@@ -15,9 +28,10 @@ cep.addEventListener("blur", (e) => {
   //fetch vai gerar uma promise. Promise é uma promessa de que algo vai acontecer.
   // se der certo, ele vai retornar o resultado. Se der errado, ele vai retornar um erro.
   // se der certo nós resolvemos a promise. Se der errado, nós rejeitamos a promise.
-  fetch(`https://viacep.com.br/ws/${search}/json/`, options)
-    .then((response) => {
-      response.json().then((result) => console.log(result));
-    })
-    .catch((e) => console.log(e.message));
+
+  const resultado = await fetch(`https://viacep.com.br/ws/${search}/json/`, options);
+  const json = await resultado.json();
+
+  showData(json);
 });
+
